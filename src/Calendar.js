fd74bdb42c8c42f35aed95e20115deb40876a29a
miguelslc <k_miguel7@hotmail.com>
2020-08-14 19:11:40 -0300

    firts commit

---
+++ src/Calendar.js
@@ -0,0 +1,65 @@
+import React, {useState } from 'react';
+import {
+    CalendarWrapper,
+    CalendarContainer,
+    PagingButton,
+    Day,
+    DayHeader
+} from './CalendarComponents'
+import moment from 'moment';
+
+export const Calendar = () => {
+    const [date, setDate] = useState(moment(new Date()));
+  
+    const createDaysOfMonth = (refDate) => {
+        const date = moment(refDate).endOf('month');
+        const lastDate = date.date();
+        const firstWeekday = date.startOf('month').day();
+
+        const calendarDays = [];
+        const today = moment();
+
+        for (let w=0; w<firstWeekday; w++) {
+            calendarDays.push(<Day key={Math.random()}/>); // empty days
+        }
+
+        for (let d=1; d<lastDate; d++) {
+            calendarDays.push(<Day key={d} today={date.date(d).isSame(today, 'day')}>{d}</Day>);
+        }
+
+        while (calendarDays.length % 7 !== 0) {
+            calendarDays.push(<Day key={Math.random()}/>);
+        }
+        
+        return calendarDays;
+    }
+
+    const prevMonth =() =>{
+        setDate(moment(date).subtract(1, 'month'));
+    }
+      
+    const nextMonth = () => {
+        setDate(moment(date).add(1, 'month'));
+    }
+
+    return (
+        <CalendarWrapper>
+                <h2>{date.format('MMMM YYYY')}</h2>
+            <div>
+                <PagingButton onClick={prevMonth}>&lt;</PagingButton>
+                <PagingButton onClick={nextMonth}>&gt;</PagingButton>
+            </div>
+            <CalendarContainer>
+                <DayHeader>DomingoLunes</DayHeader>
+                <DayHeader>Lunes</DayHeader>
+                <DayHeader>Martes</DayHeader>
+                <DayHeader>Miercoles</DayHeader>
+                <DayHeader>Jueves</DayHeader>
+                <DayHeader>Viernes</DayHeader>
+                <DayHeader>Sabado</DayHeader>
+                {createDaysOfMonth(date)}
+            </CalendarContainer>
+        </CalendarWrapper>
+    )
+
+}
